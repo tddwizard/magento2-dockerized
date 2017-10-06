4f5ccde727813ea37b3f5ded4de65f9a16ff7cd3
Fabian Schmengler <fs@integer-net.de>
2017-10-06 10:40:34 +0200

    Add troubleshooting checks to functional-tests.sh

--- bin/functional-tests.sh
+++ bin/functional-tests.sh
@@ -15,9 +15,17 @@ set -e
 echo -e "\033[33mSetting Magento configuration...\033[0m"
 ./bin/magento config:set admin/security/use_form_key 0
 ./bin/magento config:set cms/wysiwyg/enabled disabled
+./bin/magento config:set web/unsecure/base_url http://magento.lh/
+./bin/magento config:set web/secure/base_url http://magento.lh/
+
+cd dev/tests/functional
+cp ./.htaccess.sample ./.htaccess
+
+echo -e "\033[33mTroubleshooting...\033[0m"
+cd ./utils
+php -f mtf troubleshooting:check-all
 
 echo -e "\033[33mGenerating code...\033[0m"
-cd dev/tests/functional/utils
 /usr/local/bin/php generate.php
 
 echo -e "\033[33mRunning tests...\033[0m"
